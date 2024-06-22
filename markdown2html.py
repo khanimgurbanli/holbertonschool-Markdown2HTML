6e476af3212e2261680a93a0fb82bada9a274f3f
Khanim <khanimgurbanzade@gmail.com>
2024-06-22 15:41:30 +0000

    update .py file

--- markdown2html.py
+++ markdown2html.py
@@ -98,7 +98,7 @@ def convert_markdown_to_html(input_file, output_file):
 if __name__ == '__main__':
     # Check command-line arguments
     if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py <input_file.md> <output_file.html>', file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         sys.exit(1)
 
     input_file = sys.argv[1]
